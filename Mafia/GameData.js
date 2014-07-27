84d745d85936af7a077bbce55db9329c6536ec75
Greg Zhou <bottlezz2002@gmail.com>
2014-07-27 16:50:24 -0700

    commit

--- Mafia/GameData.js
+++ Mafia/GameData.js
@@ -2,7 +2,7 @@ var mocklist=[{name:"Greg",id:"1",status:1,identity:1},
 {name:"Luna",id:"2",status:0,identity:0},
 {name:"Effie",id:"3",status:0,identity:0}];
 function getPlayerList(){
-	return mocklist;
+	return playerList;
 }
 function getDeathList(){
 	var list=getPlayerList();
@@ -43,4 +43,136 @@ function getSurvivorList(){
 		}		
 	}
 	return sList;
+}
+function MarkUserDeadById(cid){
+	var list=getPlayerList();
+	for (var i = list.length - 1; i >= 0; i--) {
+		if(list[i].id==cid){
+			list[i].status=0;
+		}
+	};
+	if(cid==clientId){
+		setDead();
+	}
+		
+}
+function receivedUserlist(list){
+
+	UserList = list;
+	console.log("receivedUserlist:"+JSON.stringify(list));
+	//setHost
+	for (var i = UserList.length - 1; i >= 0; i--) {
+		var id=UserList[i].name;
+		if(UserList[i].isHost=="1" && id==clientId){
+			isHost=true;
+			showItemsByName("startButton");
+		}
+	};
+	
+}
+function recievedCallBack(object){
+	//console.log(object);
+	/*
+	if(object.type=="startGame"){
+		//everyone start the game
+		if(state!=GAME_READY){
+			return;
+		}
+
+		whoIsOn();
+		setTimeout(function(){setGameOn()},1000);*/
+	if(object.type == "peace" && inGame){
+		setGameProcessing();
+	}
+	if(object.type == "killed" && inGame){
+		MarkUserDeadById(object.value);
+		if(object.value == clientId){
+			setDead();
+//			connect.close();
+		}else{
+			setGameProcessing();
+		}
+		
+	}
+	if(object.type == "vote"){
+		voteList.push(object.value);
+		var slist= getSurvivorList();
+		if (voteList.length>=slist.length) {
+			var counts=new Object();
+			var maxIndex=null;
+			var hasEqualMax=false;
+			for (var i = voteList.length - 1; i >= 0; i--) {
+				var count=0
+				if(counts[voteList[i].votes]==undefined){
+					counts[voteList[i].votes]=1;
+					if(maxIndex==null){
+						maxIndex=voteList[i].votes;
+					}
+					count = 1;
+
+				}else{
+					count = ++counts[voteList[i].votes];
+				}
+					if(voteList[i].votes==maxIndex){
+						//if (hasEqualMax) {
+							hasEqualMax = false;
+						//};
+					}else{
+						if(count>counts[maxIndex]){
+							maxIndex = voteList[i].votes;
+							hasEqualMax = false;
+						}else if(count==counts[maxIndex]){
+							hasEqualMax = true;
+						}
+					}
+				
+			};
+			if (!hasEqualMax&&maxIndex!=null) {
+
+				MarkUserDeadById(maxIndex);
+				processTurn();
+				setGameProcessing();
+				
+			}else{
+				processTurn();
+				setGameProcessing();
+			}
+
+		};
+	}
+
+	if(object.type == "gameStart"){
+		console.log("Game start");
+
+		if(!isHost){
+			
+
+			updatePlayerList(object.value);
+
+		}
+		for (var i = playerList.length - 1; i >= 0; i--) {
+			if(playerList[i].id==clientId)
+			{
+				if( playerList[i].identity == 1){
+					isKiller = true;
+					updateUserIdentityText("Killer");
+				}	
+				else{
+					updateUserIdentityText("Civilian");
+				}
+				console.log("ingame");
+				inGame=true;
+
+
+
+			}
+			
+			
+		};
+		nextStage();
+		processGame();
+		
+	}
+	//greg added
+	
 }
