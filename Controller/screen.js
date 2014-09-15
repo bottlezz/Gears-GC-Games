63f075fe3ddce5242f79a3624aeb636bc6397d64
Greg Zhou <bottlezz2002@gmail.com>
2014-09-14 17:01:12 -0700

    coldetchange

--- Controller/screen.js
+++ Controller/screen.js
@@ -1,16 +1,16 @@
 function colDetcUser(users){
 
 	for(var i=0;i<users.length-1;i++){
-		for(var j=i+1;j<user.length;j++){
-			var dx=user[i].x-user[j].x;
-			var dy=user[i].y-user[j].y;
-			var dist=user[i].radius+user[j].radius;
+		for(var j=i+1;j<users.length;j++){
+			var dx=users[i].x-users[j].x;
+			var dy=users[i].y-users[j].y;
+			var dist=users[i].radius+users[j].radius;
 			if(Math.sqrt(dx)+Math.sqrt(dy)<Math.sqrt(dist)){
-				user[i].reverse();
-				user[j].reverse();
-				console.log("yes");
+				users[i].reverseMove();
+				users[j].reverseMove();
+	
 			}else{
-				console.log("no");
+			
 			}
 		}
 	}
