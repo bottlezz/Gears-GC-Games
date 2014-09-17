6ff7786bdc01f04348cb41263b78d4d6584a35e7
Gregory Zhou <bottlezz2002@gmail.com>
2014-09-16 17:42:55 -0700

    fix all current bug

--- Controller/screen.js
+++ Controller/screen.js
@@ -5,7 +5,7 @@ function colDetcUser(users){
 			var dx=users[i].x-users[j].x;
 			var dy=users[i].y-users[j].y;
 			var dist=users[i].radius+users[j].radius;
-			if(Math.sqrt(dx)+Math.sqrt(dy)<Math.sqrt(dist)){
+			if(dx*dx+dy*dy<dist*dist){
 				users[i].reverseMove();
 				users[j].reverseMove();
 
@@ -15,16 +15,28 @@ function colDetcUser(users){
 		}
 	}
 }
+
 function colDetcBulletOnUsers(bullets,users,onHitCallBack){
 	//onHitCallBack=function(user,bullet)
-	bullets.forEach(function(b){
+	for(i=bullets.length-1;i>=0;i--){
+		var b=bullets[i];
+		var hit=false;
+		if(b.x>1000||b.x<0||b.y>1000||b.y<0){
+			hit=true;
+		}
 		users.forEach(function(u){
 			var dx=u.x-b.x;
 			var dy=u.y-b.y;
-			var dist=u.radius+b.radius;
-			if(Math.sqrt(dx)+Math.sqrt(dy)<Math.sqrt(dist)){
+			var dist=u.radius+b.radius-1;
+			if(dx*dx+dy*dy<dist*dist){
+				hit=true;
 				onHitCallBack(u,b);
+
 			}
-		})
-	});
+		});
+		if(hit){
+			b.removeFromStage();
+			bullets.splice(i,1);
+		}
+	}
 }
